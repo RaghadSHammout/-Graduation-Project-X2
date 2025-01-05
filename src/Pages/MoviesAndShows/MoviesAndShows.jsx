35b5a7ebfec7fa511365736fa807d5ec643e5401
unknown <zeinabslman004@gmail.com>
2025-01-05 11:48:24 +0200

    update hero shows&movies

--- src/Pages/MoviesAndShows/MoviesAndShows.jsx
+++ src/Pages/MoviesAndShows/MoviesAndShows.jsx
@@ -1,7 +1,9 @@
-
+import '../../Components/Hero2/Hero2'
+import Hero2 from '../../Components/Hero2/Hero2'
 export default function MoviesAndShows() {
     return (
         <>
+       <Hero2/>
         </>
     )
 }
