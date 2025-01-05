e3c659b5e74410d6f0c2c56997f88258aaa30a26
hellen607 <hellen.haddad66@gmail.com>
2025-01-04 22:00:54 -0800

    Home-Frequently

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,11 +1,10 @@
+import "./Title.css";
 
-import './Title.css'
-
-export default function Title({title , text , size , maMargin}) {
+export default function Title({ title, text, size, maMargin }) {
   return (
-    <div className= {`${maMargin ? 'ma-50' : 'ma-80'}`}>
-      <h2 className={`ma-title ${{size} ? 'ma-size' : ''}`}>{title}</h2>
-      <p className='ma-text'>{text}</p>
+    <div className={`${maMargin ? "ma-50" : "ma-80"}`}>
+      <h2 className={`ma-title ${size ? "ma-size" : ""}`}>{title}</h2>
+      <p className="ma-text">{text}</p>
     </div>
-  )
+  );
 }
