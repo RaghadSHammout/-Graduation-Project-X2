f44d0e20cefe2ea652ef3a246a8163c32ed743ce
MonaAlnajjarmem <mona.alnajjar24@gmail.com>
2025-01-08 02:04:59 -0800

    Streaming Devices

---
+++ src/Components/StreamingDevices/StreamingDevices.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../StreamingDevices/StreamingDevices.css"
+export default function StreamingDevices() {
+  return (
+<div className='v'>
+<div className='MAContanair'>
+            <h3>We Provide you streaming experience across various devices.</h3>
+            <p>
+            With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.
+            </p>
+        </div>
+
+</div>
+      
+  )
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 
 import Frequently from "../../Components/Frequently/Frequently";
 import Hero from "../../Components/Hero/Hero";
+import Cards from "../../Components/StreamingDevices/Cards/Cards";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Frequently />
+      <Cards/>
     </>
   );
 }
