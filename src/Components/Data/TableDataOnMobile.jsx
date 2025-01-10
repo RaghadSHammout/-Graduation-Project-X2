cc7140f1f9d58b6554e261a24ed5d521c1cc9ed9
LunaQassrini996 <qassriniluna@gmail.com>
2025-01-10 22:43:38 +0200

    subsicraption table

---
+++ src/Components/Data/TableDataOnMobile.jsx
@@ -0,0 +1,41 @@
+export const TableDataOnMobile = {
+  Basic: {
+    price: "$9.99/Month",
+    FreeTrail: "7Days",
+    content:
+      "Access to a wide selection of movies and shows, including some new releases.",
+    devices: "Watch on one device simultaneously",
+    cancel: "Yes",
+    hdr: "No",
+    dolby: "No",
+    ad: "No",
+    viewing: "No",
+    fSharing: "No",
+  },
+  Standard: {
+    price: "$12.99/Month",
+    FreeTrail: "7Days",
+    content:
+      "Access to a wider selection of movies and shows, including most new releases and exclusive content",
+    devices: "Watch on Two device simultaneously",
+    cancel: "Yes",
+    hdr: "Yes",
+    dolby: "Yes",
+    ad: "Yes",
+    viewing: "Yes, for select titles.",
+    fSharing: "5 family members.",
+  },
+  Premium: {
+    price: "$14.99/Month",
+    FreeTrail: "7Days",
+    content:
+      "Access to a widest selection of movies and shows, including all new releases and Offline Viewing",
+    devices: "Watch on Four device simultaneously",
+    cancel: "Yes",
+    hdr: "Yes",
+    dolby: "Yes",
+    ad: "Yes",
+    viewing: "Yes, for all titles.",
+    fSharing: "6 family members.",
+  },
+};
