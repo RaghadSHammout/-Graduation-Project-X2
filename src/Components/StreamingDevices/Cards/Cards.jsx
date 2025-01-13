7c0bfdd9a7ac50c79971c1ea652059e9389b287f
MonaAlnajjarmem <mona.alnajjar24@gmail.com>
2025-01-13 05:41:57 -0800

    Cards Home 2

--- src/Components/StreamingDevices/Cards/Cards.jsx
+++ src/Components/StreamingDevices/Cards/Cards.jsx
@@ -6,15 +6,11 @@ import './Cards.css';
 import StreamingDevices from '../StreamingDevices';
 export default function Cards() {
   return (
-    
     <>
-
-
-   <section >
-
-   <div className='MA_card '>
+   <section className='Ma'>
+   <div className='MAA_card '>
     <StreamingDevices/>
-    <div className='MA '>
+    <div className='MAA'>
       {SkileData.map((e,index)=>{
         return(
           <Card key={index} title={e.title} img={e.img} desc={e.desc}/>
@@ -29,3 +25,4 @@ export default function Cards() {
    
   )
 }
+
