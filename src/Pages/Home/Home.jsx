854303348d1150404f308b2c8598cee5d2c844c2
Tonyos187 <tonydaoud2003@gmail.com>
2025-01-21 04:11:03 -0600

    problems with merging, commiting again

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,8 +10,8 @@ export default function Home() {
     <>
       <Hero />
       <Explore />
-      <Cards />
-      <Frequently />
+      {/* <Cards />
+      <Frequently /> */}
       <Plans />
     </>
   );
