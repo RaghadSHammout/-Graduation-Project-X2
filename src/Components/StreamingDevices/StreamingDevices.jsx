64e6db462f3b4486a706f0598f740f69311e5253
MonaAlnajjarmem <mona.alnajjar24@gmail.com>
2025-01-21 22:21:33 -0800

    Modify_StremingDevices

--- src/Components/StreamingDevices/StreamingDevices.jsx
+++ src/Components/StreamingDevices/StreamingDevices.jsx
@@ -1,17 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import "../StreamingDevices/StreamingDevices.css";
+import Title from '../Title/Title';
 
-import React from 'react'
-import "../StreamingDevices/StreamingDevices.css"
 export default function StreamingDevices() {
-  return (
-<div className='v'>
-<div className='MAContanair'>
-            <h3>We Provide you streaming experience across various devices.</h3>
-            <p>
-            With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.
-            </p>
-        </div>
+  const [displayText, setDisplayText] = useState("");
 
-</div>
-      
-  )
+  const fullText = "With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.";
+
+  const updateDisplayText = () => {
+    if (window.innerWidth <= 500) {
+      setDisplayText(fullText.slice(0, 82) + "."); 
+    } else {
+      setDisplayText(fullText);
+    }
+  };
+
+  useEffect(() => {
+    updateDisplayText(); 
+    window.addEventListener('resize', updateDisplayText);
+
+    return () => {
+      window.removeEventListener('resize', updateDisplayText); 
+    };
+  }, []);
+
+  return (
+    <div className='MAContanair'>
+      <Title
+        maMargin={'maMargin'}
+        size={'ma-size'}
+        matext={'ma-text'}
+        title={"We Provide you streaming experience across various devices."}
+        text={displayText} 
+      />
+    </div>
+  );
 }
