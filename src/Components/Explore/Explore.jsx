23aa7224a1ede30abc27f2f34637ad2c1230a5f3
RaghadSHammout <raghadshammout3010@gmail.com>
2025-01-24 21:10:21 +0300

    no thing

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -1,31 +1,8 @@
-import React from "react";
-import ExploreCard from "../ExploreCard/ExploreCard";
 import "./Explore.css";
 import CustomSlider from "../Slider/Slider";
 import cardData from "../Data/toCardData";
 
-const cardData = [
-    { title: "Action", image: "src/assets/photos/Home page images/Exploreb section card(1).png", link: "/action" },
-    { title: "Adventure", image: "src/assets/photos/Home page images/Explore section card(2).png", link: "/adventure" },
-    { title: "Comedy", image: "src/assets/photos/Home page images/Explore section card(3).png", link: "/comedy" },
-    { title: "Drama", image: "src/assets/photos/Home page images/Explore section card(4).png", link: "/drama" },
-    { title: "Horror", image: "src/assets/photos/Home page images/Explore section card(5).png", link: "/horror" },
-    { title: "Action", image: "src/assets/photos/Home page images/Exploreb section card(1).png", link: "/action" },
-    { title: "Adventure", image: "src/assets/photos/Home page images/Explore section card(2).png", link: "/adventure" },
-    { title: "Comedy", image: "src/assets/photos/Home page images/Explore section card(3).png", link: "/comedy" },
-    { title: "Drama", image: "src/assets/photos/Home page images/Explore section card(4).png", link: "/drama" },
-    { title: "Horror", image: "src/assets/photos/Home page images/Explore section card(5).png", link: "/horror" },
-    { title: "Action", image: "src/assets/photos/Home page images/Exploreb section card(1).png", link: "/action" },
-    { title: "Adventure", image: "src/assets/photos/Home page images/Explore section card(2).png", link: "/adventure" },
-    { title: "Comedy", image: "src/assets/photos/Home page images/Explore section card(3).png", link: "/comedy" },
-    { title: "Drama", image: "src/assets/photos/Home page images/Explore section card(4).png", link: "/drama" },
-    { title: "Horror", image: "src/assets/photos/Home page images/Explore section card(5).png", link: "/horror" },
-    { title: "Action", image: "src/assets/photos/Home page images/Exploreb section card(1).png", link: "/action" },
-    { title: "Adventure", image: "src/assets/photos/Home page images/Explore section card(2).png", link: "/adventure" },
-    { title: "Comedy", image: "src/assets/photos/Home page images/Explore section card(3).png", link: "/comedy" },
-    { title: "Drama", image: "src/assets/photos/Home page images/Explore section card(4).png", link: "/drama" },
-    { title: "Horror", image: "src/assets/photos/Home page images/Explore section card(5).png", link: "/horror" },
-];
+
 
 
 function Explore() {
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -11,9 +11,9 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import './Slider.css';
 //import framer-motion library
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 //import Animation.js
-import {fadeIn} from '../../Animation'
+import { fadeIn } from '../../Animation'
 
 const CustomSlider = ({ cardData, lgSize, title, text, cardGroup, upperMb, cardType }) => {
     const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 991);
@@ -40,40 +40,40 @@ const CustomSlider = ({ cardData, lgSize, title, text, cardGroup, upperMb, cardT
     const renderCard = (card, index) => {
         switch (cardType) {
             case 'CardWatchMoviesData':
-                return  <CardWatchMovies
-                key={card.id}
-                 id={card.id}
-              image={card.image}
-           duration={card.duration}
-               star={card.star}
-               Date={card.Date}
-             />;
+                return <CardWatchMovies
+                    key={card.id}
+                    id={card.id}
+                    image={card.image}
+                    duration={card.duration}
+                    star={card.star}
+                    Date={card.Date}
+                />;
             case 'cardsDataNewReleases':
                 return <Card key={index} image={card.image} releaseDate={card.releaseDate} />;
             case 'cardsDataTrending':
                 return <Card
-                            key={index}
-                            image={card.image}
-                            views={card.views}
-                            duration={card.duration}
-                            iconViews={card.iconViews}
-                            iconDuration={card.iconDuration}
-                          />;
+                    key={index}
+                    image={card.image}
+                    views={card.views}
+                    duration={card.duration}
+                    iconViews={card.iconViews}
+                    iconDuration={card.iconDuration}
+                />;
             case 'CardWatchMoviesData2':
                 return <CardWatchMovies
-                key={card.id}
-                id={card.id}
-                image={card.image}
-                duration={card.duration}
-                star={card.star}
-                Date={card.Date}
-              />;
+                    key={card.id}
+                    id={card.id}
+                    image={card.image}
+                    duration={card.duration}
+                    star={card.star}
+                    Date={card.Date}
+                />;
             case 'CardShwos':
-                return <Card key={index} image={card.image} cardShow_zq={card.cardShow_zq} 
-                viewcardShow_zq={card.viewcardShow_zq} iconDuration={card.iconDuration} duration={card.duration}
-                iconViews={card.iconViews} views={card.views}/>;
+                return <Card key={index} image={card.image} cardShow_zq={card.cardShow_zq}
+                    viewcardShow_zq={card.viewcardShow_zq} iconDuration={card.iconDuration} duration={card.duration}
+                    iconViews={card.iconViews} views={card.views} />;
             case 'ReleasedShwos':
-                return <Card key={index} image={card.image} cardShow_zq={card.cardShow_zq}  viewcardShow_zq={card.viewcardShow_zq} iconDuration={card.iconDuration} duration={card.duration} durationcard_zq={card.durationcard_zq} iconViews={card.iconViews} views={card.views} />;
+                return <Card key={index} image={card.image} cardShow_zq={card.cardShow_zq} viewcardShow_zq={card.viewcardShow_zq} iconDuration={card.iconDuration} duration={card.duration} durationcard_zq={card.durationcard_zq} iconViews={card.iconViews} views={card.views} />;
             case 'movie':
                 return <MovieCard key={index} title={card.title} image={card.image} link={card.link} />;
             case 'explore':
@@ -93,13 +93,13 @@ const CustomSlider = ({ cardData, lgSize, title, text, cardGroup, upperMb, cardT
 
     return (
         <div>
-            <motion.div 
-              variants={fadeIn("right" , 0.2)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{once:false , amount:0.7}}
-            className="slider-upper-div">
-            <div className={`slider-upper-div ${upperMb}`}>
+            <motion.div
+                variants={fadeIn("right", 0.2)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                className="slider-upper-div">
+                <div className={`slider-upper-div ${upperMb}`} />
                 <Title title={title} text={text} size="to-title" matext="to-text" />
                 <div className="custom-arrows">
                     <button className="prev-arrow">
@@ -136,16 +136,16 @@ const CustomSlider = ({ cardData, lgSize, title, text, cardGroup, upperMb, cardT
             >
                 {groupedCards.map((group, groupIndex) => (
                     <SwiperSlide key={groupIndex}>
-                        <motion.div 
-                         variants={fadeIn("left" , 0.2)}
-                         initial="hidden"
-                         whileInView={"show"}
-                         viewport={{once:false , amount:0.62}}
-                        className="card-group">
-                        <div className={`${cardGroup}`}>
-                            {group.map((card, cardIndex) => (
-                                renderCard(card, cardIndex)
-                            ))}
+                        <motion.div
+                            variants={fadeIn("left", 0.2)}
+                            initial="hidden"
+                            whileInView={"show"}
+                            viewport={{ once: false, amount: 0.62 }}
+                            className="card-group">
+                            <div className={`${cardGroup}`}/>
+                                {group.map((card, cardIndex) => (
+                                    renderCard(card, cardIndex)
+                                ))}
                         </motion.div>
                     </SwiperSlide>
                 ))}
