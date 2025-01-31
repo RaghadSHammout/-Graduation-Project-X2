f1faeeda2bfc21827cf18a212522aba7763279b5
RaghadSHammout <raghadshammout3010@gmail.com>
2025-01-31 23:14:24 +0300

    compressed images, created About Us page, fixed 404 page and navbar, edited "Who Are We" component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import MoviesAndShows from "./Pages/MoviesAndShows/MoviesAndShows";
 import Subscription from "./Pages/Subscription/Subscription";
@@ -8,12 +8,13 @@ import Footer from "./Components/Footer/Footer.jsx";
 import Navbar from "./Components/Navbar/Navbar";
 import MoviesPageOpenR from "./Pages/MoviesPageOpenR/MoviesPageOpenR.jsx";
 import ShowsPageOpenR from "./Pages/ShowsPageOpenR/ShowsPageOpenR.jsx";
-
 import HashLoader from "react-spinners/HashLoader";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage.jsx";
-
+import AboutUs from "./Pages/AboutUs/AboutUs.jsx";
+import './App.css';
 export default function App() {
   const [loading, setLoading] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     setLoading(true);
@@ -24,51 +25,58 @@ export default function App() {
 
   const color = "#e50000";
 
+  const isErrorPage = ![
+    "/", 
+    "/MoviesAndShows", 
+    "/MoviesPageOpenR/movie/:id", 
+    "/ShowsPageOpenR/show/:id", 
+    "/Subscription", 
+    "/Support",
+    "/AboutUs" 
+  ].includes(location.pathname);
 
+  
   return (
     <>
       {loading && (
         <div className="loader"
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: 999800999,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "rgba(6, 6, 6, 0.78)",
-          }}
+          // style={{
+          //   position: "fixed",
+          //   top: 0,
+          //   left: 0,
+          //   width: "100%",
+          //   height: "100%",
+          //   zIndex: 99999999,
+          //   display: "flex",
+          //   justifyContent: "center",
+          //   alignItems: "center",
+          //   backgroundColor: "rgba(6, 6, 6, 0.78)",
+          // }}
         >
           <HashLoader
             color={color}
             loading={loading}
             size={100}
-            cssOverride={{
-              position: "absolute",
-            }}
+            cssOverride={{ position: "absolute" }}
             aria-label="Loading Spinner"
             data-testid="loader"
           />
         </div>
       )}
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/MoviesAndShows" element={<MoviesAndShows />} />
-          <Route path="/MoviesPageOpenR/movie/:id" element={<MoviesPageOpenR />} />
-          <Route path="/ShowsPageOpenR/show/:id" element={<ShowsPageOpenR />} />
-          <Route path="/Subscription" element={<Subscription />} />
-          <Route path="/Support" element={<Support />} />
-          <Route path="/*" element={<ErrorPage/>} />
-        </Routes>
-        <Footer />
-      </>
 
-    
+      {!isErrorPage && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/MoviesAndShows" element={<MoviesAndShows />} />
+        <Route path="/MoviesPageOpenR/movie/:id" element={<MoviesPageOpenR />} />
+        <Route path="/ShowsPageOpenR/show/:id" element={<ShowsPageOpenR />} />
+        <Route path="/Subscription" element={<Subscription />} />
+        <Route path="/Support" element={<Support />} />
+        <Route path="/AboutUs" element={<AboutUs />} /> 
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+
+      {!isErrorPage && <Footer />}
     </>
   );
 }
--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -1,21 +1,19 @@
-import { Button } from "bootstrap"
 import "./Error404.css"
 export default function Error404() {
   return (
     <div>
-      <div className="background">
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-      </div>
+   <div className="background">
+  <div className="circle"></div>
+  <div className="circle"></div>
+  <div className="circle"></div>
+  <div className="circle"></div>
+</div>
 
-      <div className="errorPage">
-        <h1>404</h1>
-        <h2>Oops! Page Not Found</h2>
-        <p>Sorry, but the page you’re looking for doesn’t exist or has been moved.</p>
-        <Button    text={"RETURN HOME"}/>
-      </div>
+<div className="errorPage">
+  <h1>404</h1>
+  <h2>Oops! Page Not Found</h2>
+  <p>Sorry, but the page you’re looking for doesn’t exist or has been moved.</p>
+</div>
     </div>
-  )
+       )
 }
