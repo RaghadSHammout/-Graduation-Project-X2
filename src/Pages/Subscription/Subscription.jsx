eb91e16e350f2cdf29bcaf489ea2958c8bc73637
RaghadSHammout <raghadshammout3010@gmail.com>
2025-02-06 11:02:33 +0300

    remove comment

--- src/Pages/Subscription/Subscription.jsx
+++ src/Pages/Subscription/Subscription.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Plans from '../../Components/Plans/Plans'
 import ComparePlansTable from '../../Components/ComparePlansTable/ComparePlansTable'
-// import CastCarousel from '../../Components/CastCarousel/CastCarousel';
-// import { ShowActorsImages } from '../../Components/Data/ShowsActorsImages';
+
 export default function Subscription() {
   return (
     <>
